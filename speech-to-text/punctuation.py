fcf0299812da19a1bb45d3e1e48f2d48d715af3e
charusm18 <charusm18@gmail.com>
2019-11-02 14:49:20 -0400

    changed credential path for punctuation

--- speech-to-text/punctuation.py
+++ speech-to-text/punctuation.py
@@ -2,7 +2,7 @@ from google.cloud import speech_v1p1beta1
 import io
 import os
 
-credential_path = "C:\\Users\\charu\\r2-chatbot\\api_keys\\SpeechToTextCuCup-538350526ced.json"
+credential_path = "..\\api_keys\\SpeechToTextCuCup-538350526ced.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 def sample_recognize(local_file_path):
