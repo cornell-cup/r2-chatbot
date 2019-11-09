5e29f7968f264a20b7327ec2b60bd6b927b74e30
charusm18 <charusm18@gmail.com>
2019-11-09 12:53:21 -0500

    Added code to only listen to one line of speech

--- speech-to-text/live_streaming.py
+++ speech-to-text/live_streaming.py
@@ -10,7 +10,7 @@ from google.cloud.speech import types
 import pyaudio
 from six.moves import queue
 
-credential_path = "..\\api_keys\\SpeechToTextCuCup-538350526ced.json"
+credential_path = "../api_keys/SpeechToTextCuCup-538350526ced.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 # Audio recording parameters
@@ -136,6 +136,34 @@ def listen_print_loop(responses):
 
             num_chars_printed = 0
 
+def returnResponseString(responses):
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+        transcript = result.alternatives[0].transcript
+        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + '\r')
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+
+        else:
+            return(transcript + overwrite_chars)
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            # if re.search(r'\b(exit|quit)\b', transcript, re.I):
+            #     print('Exiting..')
+            #     break
+
+            #num_chars_printed = 0
+
 
 def main():
     # See http://g.co/cloud/speech/docs/languages
@@ -158,9 +186,8 @@ def main():
                     for content in audio_generator)
 
         responses = client.streaming_recognize(streaming_config, requests)
-
         # Now, put the transcription responses to use.
-        listen_print_loop(responses)
+        print(returnResponseString(responses))
 
 
 if __name__ == '__main__':
