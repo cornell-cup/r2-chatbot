51ccb01d88f929e3ab576c0441840a13b6a7fae1
wertylop5 <slin55055@gmail.com>
2019-11-16 15:17:39 -0500

    working on smarter topic detection using nltk

---
+++ tests/pos_test.py
@@ -0,0 +1,46 @@
+import re
+import nltk
+
+lines = [
+    #need to use a possesive determiner
+    #"what's the weather",
+    #"what is today's weather",
+    #"what is the weather today",
+    #"what is the weather like today",
+
+    #"his car is actually his mom's car",
+    #"his car is actually his mom's",
+    #"his car seat is actually his mom's car seat"
+    #"Mary saw the cat sit on the mat"
+]
+with open("tests/question_keyword_tests.txt") as f:
+    for line in f:
+        lines.append(line)
+
+expression = r"""
+POS_DT: {<NN.*><POS><NN.*>*}
+NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
+"""
+'''
+expression = r"""
+    NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
+    PP: {<IN><NP>}               # Chunk prepositions followed by NP
+    VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
+"""
+'''
+
+for line in lines:
+    #break sentence into tokens (ex: words)
+    tokenized_line = nltk.word_tokenize(line)
+
+    #mark tokens with part of speech
+    pos_tagged = nltk.pos_tag(tokenized_line)
+
+    #the type of phrase we want to detect in a sentence
+    parser = nltk.RegexpParser(expression)
+
+    #look through the tagged tokens for the phrase
+    parsed_text = parser.parse(pos_tagged)
+
+    #print(parsed_text)
+    parsed_text.draw()
--- util/detect_question.py
+++ util/detect_question.py
@@ -52,6 +52,23 @@ def is_question(line):
             list(tree.subtrees(
                 filter=lambda tree: tree.label() == "question"))) > 0
 
+def match_regex_and_keywords(line, exp, keywords=None):
+    '''
+    Attempts to first match the nltk regular expression to the
+    specified sentence. Then, for each matched chunk, determines whether
+    any keywords are in the chunk
+
+    @param line: The sentence to check
+    @param expression: A raw string containing the regular expression
+            chunk to match. This is utilizing nltk's regex parser
+    @param keywords: Optional. An array containing keywords to match.
+            Can also require keywords to appear only in certain chunks.
+
+    @return: A list of matched chunks. If keywords was specified, will
+            only return chunks where a keyword was contained in it
+    '''
+    tree = parse(line, exp)
+
 if __name__ == "__main__":
     with open("../tests/not_questions.txt") as f:
         for line in f:
