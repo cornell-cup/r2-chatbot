bdda33e7ed71769c5c38de30326ee6368c26a652
Rishi Malhotra <rishimalhotra@dhcp-hol-4188.eduroam.cornell.edu>
2019-11-16 12:53:20 -0500

    Added the new api key

---
+++ speech-to-text/Speech_to_Text.py
@@ -0,0 +1,12 @@
+{
+ "type": "service_account",
+ "project_id": "speech-to-text-1573926410742",
+ "private_key_id": "f685dd05bfd5f00e135cdf7e8609dff17cf208cf",
+ "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDrJZYSHEWFBnh1\niReEgeDM4EsEBnRcm+t5Pm4T4SFOcEEdeTNLE+X7IWepG/JRV+I1DBNFn+9i1rOl\nmtXofu6XiMxE/RyL6AYLLC7+H0NiEGl45xzROu0EPlik3emy9ewEd+rSOEkOLyjF\nbHHmML1FRq56649z+ZyHbukJ4CQ1HiuTqh4Dokv13hkfS28OywtXjlbtB8qWUYTz\nM543raP/h7As29LRkEZcODw9nnaJuFGMl1r1BT33oo7q96D7nXhaqkNuHqHb3/h5\nEq3Kv0Q0YVzSRPPIN+zf08FTe1KPteL3AFazPHB3bzQKaxlWDtsayruVdtrX51t0\nv5CIFObbAgMBAAECggEAClE6Xe4OoNERXPrYZNB8CBE5SWJtHZkbwlSaaNXia7oi\nKURlm0AywYuMLE/VGHuWKcSvXckwLDKGbFg4VXyoRE7Uw5wlcgAKr9fO8gh2IOne\nbtcZHRcMSjjgE8/GzrrakbGfRH0Cu5QXIl5pOvPdghohUrReNXT4yLp1kCDdBdWJ\nBRb3uIdebj5a3HXSzgj+t56SyEE1jNNtiv2UDg0NA72Bc+DehNOIp/h3a9CdmUHE\nKjNjj3Vc59PfOWdSGUtM4izZDLHcoRApf2z+qd3Y/YwfZ+J4D1mySDGVMo+qBF4L\nRdYj8lQyglEbzNUOWzfp2H+MioBMA+cub5fEyLkMOQKBgQD2coCxUEBIs/IGkknd\nFaKwLihGTCecyH7+SN6aIXZDMI6kmYU5QeW6+nrkrng7BxWHpJyEkghgQUShJBa9\nb5A9ChPqeTMM3dPpMrGVLsjz7mhhHgc4bo1LxQ9w4cbk1mBFRA4v8PfPoozCi77/\nl0iSFiW6ptcTCSQFaGRfpSC5QwKBgQD0QvL00yhqTCKMW4jegWNnWS5MNBWRjoaB\n/6eV2832knn8LxvOVl86dHgWf8nrClOa8mQtx/8xYbiZZfeuB/FwbBOLnB9pduBu\nNyf6ClsbIgord4kCi1JB2rkmofhk/xUaTegnskZVjqGIEZTNjqzoExOSqp8FtXYp\nxuVq+mIWiQKBgGWHIHtr2z/YWhHY/HMHMcGxEkh7oSoW8zJg5MvMk0L04x7uXBMN\nkc3o2O4S7lcrAPSjnROEccztto9IJdIe/uYWm8PR8HURgfalOsGBx6mugvPXghP+\nf1lCsqzHZr/pjTmb4thfRkgU6DZRifsT4QLF+v8dMqeV8XAE5Kz+gy+vAoGAFHNS\nghr3xEaRkezL/c0ZdHqr/KdTsvca3eF4t3xKtz02R13uVw02Nd+wejnCvxIDQ/LN\nXmPjQtAqs3UtEn4Jwd7amBK/MoZIbhHiH/YkAJaOAWulpffiMC9O1yQydspAiw5A\nxKoRZ3Jkpc7LBZbPdTK7rcjpPYIe3YqQhK9n7PkCgYEAzo/Ao/R8mGPz5ys+KIao\n2S7bHRz9oq979ugyisvPbLxse2Vm41hEHzgZHqI0Kj4jmx9tbWvjxsTT3Ls4GQhS\n07Ew0mLJ3cWmJHLpZjYA+HLO2rYtgWIVuG9jAycctw9uXAgIvI6hfoMIgNKic6qe\n5qVlnz15YQ1XgLQkgp3O4p0=\n-----END PRIVATE KEY-----\n",
+ "client_email": "starting-account-1ljs67xy4viq@speech-to-text-1573926410742.iam.gserviceaccount.com",
+ "client_id": "100679714422750955808",
+ "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+ "token_uri": "https://oauth2.googleapis.com/token",
+ "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+ "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/starting-account-1ljs67xy4viq%40speech-to-text-1573926410742.iam.gserviceaccount.com"
+}
