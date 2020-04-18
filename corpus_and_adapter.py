e6e2c168721f77439bb72ad72612021da59e4f6b
sahar-sami <sahar.sami115@gmail.com>
2020-04-18 14:28:30 -0400

    combined corpus and adapter functionality

---
+++ corpus_and_adapter.py
@@ -0,0 +1,36 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.response_selection import get_most_frequent_response
+
+# Uncomment the following lines to enable verbose logging
+#import logging
+#logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+bot = ChatBot(
+    'Terminal',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'logic_adapters.weather_adapter.WeatherAdapter',
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='sqlite:///database.db',
+    response_selection_method=get_most_frequent_response
+)
+
+corpus_trainer = ChatterBotCorpusTrainer(bot)
+corpus_trainer.train('chatterbot.corpus.english.greetings', "./custom_corpus.yaml")
+
+print('Type something to begin...')
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        user_input = input()
+        bot_response = bot.get_response(user_input)
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
