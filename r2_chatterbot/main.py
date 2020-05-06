8b433de083bd523ada4ccaf46aec9edc5d7977ad
wertylop5 <slin55055@gmail.com>
2020-05-05 20:23:53 -0400

    tested weather with c1c0 filtering. renamed required speech to text key

--- r2_chatterbot/main.py
+++ r2_chatterbot/main.py
@@ -17,7 +17,7 @@ import corpus_and_adapter
 import re
 
 print(os.getcwd())
-credential_path = "api_keys/Speech to Text-bef030531cd1.json"
+credential_path = "api_keys/speech_to_text.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 utils.set_classpath()
@@ -32,10 +32,7 @@ def main():
         if "quit" in speech or "stop" in speech:
             break
         if("cico" in speech.lower() or "kiko" in speech.lower() or "c1c0" in speech.lower()):
-            # filter out cico since it messes with location detection
-            for s in [r"((k|K)((i|1)k(o|0))|((c|C)(i|1)c(o|0)))", r"(h|H)ey"]:
-                speech = re.sub(s, "", speech)
-            speech = speech.strip(".,?! ")
+            speech = utils.filter_cico(speech)
             
             if face_recognition.isFaceRecognition(speech):
                 print(face_recognition.faceRecog(speech))
