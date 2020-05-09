4595d00bb21f7094209ea3064018b13e71098ebe
yz366 <yz366@cornell.edu>
2020-05-09 13:06:33 -0400

    first iteration of passing things through

--- r2_chatterbot/main.py
+++ r2_chatterbot/main.py
@@ -7,8 +7,8 @@ from util import path_planning
 from util import object_detection
 from util import face_recognition
 from util import utils
-from util.api import weather
-from util.api import restaurant
+#from util.api import weather
+#from util.api import restaurant
 from playsound import playsound
 import re
 import sys
@@ -16,9 +16,17 @@ import os
 import corpus_and_adapter
 import re
 
+# for flask setup
+import requests
+import json
+import io
+import socket
+
 print(os.getcwd())
 credential_path = "api_keys/speech_to_text.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+url = "http://18.216.143.187/"
 
 utils.set_classpath()
 
@@ -35,17 +43,21 @@ def main():
         if("cico" in speech.lower() or "kiko" in speech.lower() or "c1c0" in speech.lower()):
             # filter out cico since it messes with location detection
             speech = utils.filter_cico(speech)
-
+            
             if face_recognition.isFaceRecognition(speech):
-                print(face_recognition.faceRecog(speech))
+                print(face_recognition.faceRecog(speech)) 
+                # task is to transfer over to facial recognition client program
             elif path_planning.isLocCommand(speech.lower()):
                 print("Move command (itemMove, direction, moveAmmount): ")
                 print(path_planning.pathPlanning(speech.lower()))
+                # task is to transfer over to path planning on the system
             elif object_detection.isObjCommand(speech.lower()):
                 print("Object to pick up: " + object_detection.object_parse(speech.lower()))
+                # task is to transfer over to object detection on the system
             else:
                 # we don't want the text to be lowercase since it messes with location detection
                 print(corpus_and_adapter.response_from_chatbot(speech))
+                # send this element to AWS for response generation
 
 if __name__ == '__main__':
     #playsound('sounds/cicoremix.mp3')
