dec9a1e8f69f4606c7d55e73867cf369e1aca02e
Sahar Sami <sahar.sami115@gmail.com>
2020-09-19 13:15:41 -0400

    added training function to text classification file

--- r2_chatterbot/topic_classifier.py
+++ r2_chatterbot/topic_classifier.py
@@ -1,48 +1,61 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 import pickle
+import json
+
 
 def import_data():
-  '''
-  Imports data for training a model and converts it to a DF.
-  
-  '''
-  
-def fit_tfidf(X, training):
-  '''
-  Produces features from text using a TfIDF score. 
-  Inputs:
-    X (list): List containing the question inputs
-    training (bool): True if training a model, false if just converting
-    a single question
-  Output: Array of features
-  '''
+    '''
+    Imports data for training a model and converts it to a DF.
+
+    '''
 
 
-def get_topic(question, model): 
-  '''
-  Returns the topic of a question (in words)
-  Inputs:
-    question (string): the question
-    model: the trained model
-  '''
-
-def train():
-  '''
-  Splits the data into training set and testing set.
-  Gets tfidf features from training set and trains model.
-  Saves model as a file using pickle.
-  Still need to finish this declaration
-  '''
-  
-  
+def get_topic(question, model):
+    '''
+    Returns the topic of a question (in words)
+    Inputs:
+      question (string): the question
+      model: the trained model
+    '''
 
 
+def train(df):
+    '''
+    Splits the data into training set and testing set.
+    Fits training data to combined tfidf vectorizer and multinomial logistic regression model
+    Saves model to file 'text_classification.sav'. 
+    Requires:
+        - df: DataFrame -> dataframe that holds all question data. Should have 
+        at least two columns, 'question_processed' and 'topic_code'. 
+    Returns:
+        - Test features, test labels, and model.
+    '''
+    X = df['question_processed']
+    y = df['topic_code']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=.2, random_state=101)
+    log_reg_model = LogisticRegression(C=100, solver='newton-cg')
+    pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer()),
+        ('clf', log_reg_model)
+    ])
+    pipeline.fit(X_train, y_train)
+    filename = 'text_classification.sav'
+    pickle.dump(pipeline, open(filename, 'wb'))
+    return X_test, y_test, pipeline
 
 
 if __name__ == '__main__':
-  #this should be run whenever the model needs to be trained
-  #import data, process, convert to DF
-  #perform tfidf vectorization and save the vectorizer
-  #train model on tfidf features
-  #save model as a file
-  #test model
+    print("Importing data...")
+    df = import_data()
+    print("Data imported and converted. Model Training...")
+    X_test, y_test, model = train(df)
+    print("Model trained and saved. Testing model...")
+    pred = model.predict(X_test)
+    print("Classification Report:")
+    print(classification_report(y_test, pred))
+    pass
