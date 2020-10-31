79f03ea032be96c6adfa6f26c94e6190e1518b13
Sahar Sami <sahar.sami115@gmail.com>
2020-10-31 14:22:52 -0400

    finished function to process path planning commands

--- r2_chatterbot/util/path_planning.py
+++ r2_chatterbot/util/path_planning.py
@@ -1,6 +1,8 @@
 import utils
 import re
 import nlp_util
+import nltk
+import math
 
 
 def isLocCommand(text):
@@ -14,15 +16,22 @@ def isLocCommand(text):
     @param text: The sentence to check (must be in lowercase)
     @return: A boolean. True indicates that the input is a locomotion command
     '''
-    r_expr = r"""
-    VP: {(<JJ>)?<NN.*>+(<VB.*>)?<RB|VBD|JJ|CD>(<CD|JJ>)?}
-    """
+    # r_expr = r"""
+    # VP: {(<JJ>)?<NN.*>+(<VB.*>)?<RB|VBD|JJ|CD>(<CD|JJ>)?}
+    # """
+
+    if text == "stop":
+        return True
+
+    # TODO: Make these include more than just "to the", should also work for "in the"
     r_expr2 = r"""
     NumberFirst: {(<CD><NNS>?(<TO><DT>)?<RB|VBD|JJ|VBP|NN>)}
     DirectionFirst: {((<TO><DT>)?<RB|VBD|JJ|VBP|NN><CD><NNS>?)}
     """
-    target_verbs = ["move", "spin", "rotate", "turn", "go", "drive"]
-    target_words = ["degrees", "left", "right", "forward", "backward"]
+    target_verbs = ["move", "spin", "rotate",
+                    "turn", "go", "drive", "stop", "travel"]
+    target_words = ["degrees", "left", "right", "forward", "backward",
+                    "clockwise", "counterclockwise"]
 
     locPhrase, keywords = nlp_util.match_regex_and_keywords(
         text, r_expr, target_words)
@@ -30,6 +39,98 @@ def isLocCommand(text):
         if verb in text and len(locPhrase) > 0:
             return True
     return False
+
+
+def get_loc_params(phrase):
+    if locphrase[0].label() == "NumberFirst":
+        number = locphrase[0][0]
+        unit = locphrase[0][1]
+        direction = locphrase[0][-1]
+    else:
+        direction = locphrase[0][-3]
+        number = locphrase[0][-2]
+        unit = locphrase[0][-1]
+    return number, unit, direction
+
+
+def process_loc(text):
+
+    mode = 0  # 0 for garbage, 1 for turn, 2 for move
+
+    r_expr2 = r"""
+    NumberFirst: {(<CD><NNS>?(<TO><DT>)?<RB|VBD|JJ|VBP|NN>)}
+    DirectionFirst: {((<TO><DT>)?<RB|VBD|JJ|VBP|NN><CD><NNS>?)}
+    """
+    target_verbs = ["move", "spin", "rotate",
+                    "turn", "go", "drive", "stop", "travel"]
+    target_words = ["degrees", "left", "right", "forward", "backward",
+                    "clockwise", "counterclockwise"]
+
+    locPhrase, keywords = nlp_util.match_regex_and_keywords(
+        text, r_expr2, target_words)
+
+    tagged_list = nltk.pos_tag(nltk.word_tokenize(text))
+    verbs_and_nouns = [tup[0]
+                       for tup in tagged_list if tup[1] == 'NN' or tup[1] == 'VB']
+
+    for verb in verbs_and_nouns:
+        if verb == "stop":
+            return ("stop", 0)
+        elif verb in ["turn", "spin", "rotate"]:
+            mode = 1
+        elif verb in ["move", "go", "drive", "travel"]:
+            mode = 2
+
+    if mode == 1:
+        number, unit, direction = get_loc_params(locphrase[0])
+        if unit == "radian" or "radians":
+            number = number * 180 / math.pi
+        if direction == "left":
+            direction = "counterclockwise"
+        elif direction == "right":
+            direction = "clockwise"
+        # direction: clockwise or counterclockwise, number in degrees
+        return (direction, number)
+
+    if mode == 2:
+        if len(locphrase) > 1:
+            x, y = 0
+            for each phrase in locphrase:
+                number, unit, direction = get_loc_params(phrase):
+                if unit == "feet" or "foot":
+                    number = number * 0.3048
+                if direction1 == "forward":
+                    y += number
+                elif direction == "left":
+                    x -= number
+                elif direction == "right":
+                    x += number
+                elif direction == "backward":
+                    y -= number
+            return (x, y)
+        else:
+            number, unit, direction = get_loc_params(locphrase[0])
+            if unit == "feet" or "foot":
+                number = number * 0.3048
+            if direction == "forward":
+                return ("move forward", number)
+            elif direction == "left":
+                return (-number, 0)
+            elif direction == "right":
+                return (number, 0)
+            elif direction == "backward":
+                return (0, -number)
+            else:
+                return (0, 0)
+
+        # 2. check if turn --> movement
+        # 3. check if moving
+        # a. check if 2 command --> coordinate
+        # b. check if 1 command
+        # i. check if forward --> movement
+        # ii. check if other dir --> coordinate
+
+    return locPhrase, keywords
 
 
 def pathPlanning(text):
