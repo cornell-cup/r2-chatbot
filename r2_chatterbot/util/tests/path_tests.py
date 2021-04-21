9d0ca7f42b74dd717120d6af546d1d8c9a06a2d9
Junyu Wang <jw838@cornell.edu>
2021-04-21 17:34:23 -0400

    testcase update

--- r2_chatterbot/util/tests/path_tests.py
+++ r2_chatterbot/util/tests/path_tests.py
@@ -21,18 +21,18 @@ DESCRIPTION: {(<NNP>)+}
 
 
 for line in lines:
-    #break sentence into tokens (ex: words)
+    # break sentence into tokens (ex: words)
     tokenized_line = nltk.word_tokenize(line)
 
-    #mark tokens with part of speech
+    # mark tokens with part of speech
     pos_tagged = nltk.pos_tag(tokenized_line)
 
-    #print(pos_tagged)
+    # print(pos_tagged)
 
-    #the type of phrase we want to detect in a sentence
+    # the type of phrase we want to detect in a sentence
     parser = nltk.RegexpParser(r_expr)
 
-    #look through the tagged tokens for the phrase
+    # look through the tagged tokens for the phrase
     parsed_text = parser.parse(pos_tagged)
 
     print(parsed_text.draw())
