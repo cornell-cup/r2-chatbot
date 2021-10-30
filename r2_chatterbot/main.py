923f997a490d5a1556aaaf0217835dbdfe1e4a9a
Junyu Wang <jw838@cornell.edu>
2021-10-30 13:53:30 -0400

    aws migration

--- r2_chatterbot/main.py
+++ r2_chatterbot/main.py
@@ -8,27 +8,32 @@ from util import live_streaming
 from util import nlp_util
 from util import keywords
 from util import make_response
+
 # from util import playtrack
 from util import path_planning
 from util import object_detection
 from util import face_recognition
 from util import utils
+
 # from util import sentiment
 from util.api import weather
 from util.api import restaurant
+
 # from topic_classifier import get_topic
 from playsound import playsound
 import re
 import sys
 import os
+
 # import corpus_and_adapter
 # from question_answer import get_answer
 import time
 import nltk
 import random
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+
+nltk.download("averaged_perceptron_tagger")
+nltk.download("maxent_ne_chunker")
+nltk.download("words")
 
 # for flask setup
 
@@ -36,28 +41,29 @@ USE_AWS = True
 
 print(os.getcwd())
 credential_path = "api_keys/speech_to_text.json"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
 
 # url = "http://18.216.143.187/"
 url = "http://3.13.116.251/"
 chatbot_qa_route = "chatbot_qa/"
 sentiment_qa_route = "sentiment_analysis/"
+weather_restaurant_route = "weather_restaurant"
 # route = "c1c0_aws_flask/r2-chatbot/r2_chatterbot_server/"
 
 utils.set_classpath()
 
 # saved answers setup
-if os.path.exists('./saved_answers.csv'):
-    saved_answers = pd.read_csv('./saved_answers.csv').to_dict()
+if os.path.exists("./saved_answers.csv"):
+    saved_answers = pd.read_csv("./saved_answers.csv").to_dict()
 else:
     saved_answers = {}
 
 # bad punctuation for cleaning string (no question mark)
-punctuations = '''!()-[]{};:'"\,<>./@#$%^&*_~'''
+punctuations = """!()-[]{};:'"\,<>./@#$%^&*_~"""
 
 
 def no_punct(string):
-    no_punct = ''
+    no_punct = ""
     for char in string:
         if char not in punctuations:
             no_punct += char
@@ -81,8 +87,11 @@ def main():
         if "quit" in speech.lower() or "stop" in speech.lower():
             break
 
-        if("cico" in speech.lower() or "kiko" in speech.lower() or "c1c0" in speech.lower()) and \
-                ("hey" in speech.lower()):
+        if (
+            "cico" in speech.lower()
+            or "kiko" in speech.lower()
+            or "c1c0" in speech.lower()
+        ) and ("hey" in speech.lower()):
             # filter out cico since it messes with location detection
             question, question_type = nlp_util.is_question(speech)
             speech = utils.filter_cico(speech) + " "
@@ -94,7 +103,9 @@ def main():
             elif not question and path_planning.isLocCommand(no_punct(speech.lower())):
                 response = path_planning.process_loc(no_punct(speech.lower()))
                 # task is to transfer over to path planning on the system
-            elif (not question or question_type == "yes/no question") and object_detection.isObjCommand(speech.lower()):
+            elif (
+                not question or question_type == "yes/no question"
+            ) and object_detection.isObjCommand(speech.lower()):
                 pick_up = object_detection.object_parse(speech.lower())
                 if pick_up:
                     response = "Object to pick up: " + pick_up
@@ -105,18 +116,37 @@ def main():
                 if question:
                     data = keywords.get_topic(speech, parse_location=False)
                     keywords.modify_topic_data(data, parse_location=True)
-                    if "name" in data.keys() and data["name"] == "weather":
-                        keywords.modify_topic_data(data, parse_location=True)
-                        api_data = weather.lookup_weather_today_city(
-                            data["info"]["location"]["name"])
-                        response = make_response.make_response_api(
-                            data, api_data)
-                    elif "name" in data.keys() and data["name"] == "restaurant":
-                        keywords.modify_topic_data(data, parse_location=True)
-                        api_data = restaurant.lookup_restaurant_city(
-                            data["info"]["location"]["name"])
-                        response = make_response.make_response_api(
-                            data, api_data)
+                    if "name" in data.keys() and (
+                        data["name"] == "weather" or data["name"] == "restaurant"
+                    ):
+                        if USE_AWS:
+                            response = requests.get(
+                                url + weather_restaurant_route,
+                                params={"speech": speech},
+                            )
+                            if response.ok:
+                                response = response.text
+                                # print(response)
+                                response = ast.literal_eval(response)
+                                answers = response["answers"][0]["answer"]
+                                # for i in range(len(answers)):
+                                #     print(f'Answer {i}: {answers[i]}')
+
+                                # this is response with highest score, we need to keep all answers somewhere
+                                # response = response['answers'][0]['answer']
+                                response = response["answers"][0]["answer"]
+                            else:
+                                raise Exception("bad request")
+                        elif data["name"] == "weather":
+                            api_data = weather.lookup_weather_today_city(
+                                data["info"]["location"]["name"]
+                            )
+                            response = make_response.make_response_api(data, api_data)
+                        elif data["name"] == "restaurant":
+                            api_data = restaurant.lookup_restaurant_city(
+                                data["info"]["location"]["name"]
+                            )
+                            response = make_response.make_response_api(data, api_data)
                     else:
                         # Q/A section
 
@@ -129,22 +159,24 @@ def main():
                         #     response = all_good_answers[idx]
                         # else:
                         if USE_AWS:
-                            response = requests.get(url+chatbot_qa_route, params={'speech': speech})
+                            response = requests.get(
+                                url + chatbot_qa_route, params={"speech": speech}
+                            )
                             if response.ok:
                                 response = response.text
                                 # print(response)
                                 response = ast.literal_eval(response)
-                                answers = response['answers'][0]['answer']
-                                    # for i in range(len(answers)):
-                                    #     print(f'Answer {i}: {answers[i]}')
+                                answers = response["answers"][0]["answer"]
+                                # for i in range(len(answers)):
+                                #     print(f'Answer {i}: {answers[i]}')
 
-                                    # this is response with highest score, we need to keep all answers somewhere
-                                    # response = response['answers'][0]['answer']
-                                response = response['answers'][0]['answer']
+                                # this is response with highest score, we need to keep all answers somewhere
+                                # response = response['answers'][0]['answer']
+                                response = response["answers"][0]["answer"]
                             else:
-                                raise Exception('bad request')
+                                raise Exception("bad request")
                         else:
-                                # response = get_answer(speech)
+                            # response = get_answer(speech)
                             response = "go to question-answering"
                         # if type(response) == list:
                         #     i = 0
@@ -182,16 +214,17 @@ def main():
                 else:
                     if USE_AWS:
                         response = requests.get(
-                            url + sentiment_qa_route, params={'speech': speech})
+                            url + sentiment_qa_route, params={"speech": speech}
+                        )
                         if response.ok:
-                            print('AWS worked')
+                            print("AWS worked")
                             response = response.text
                     else:
-                        print('AWS was not used')
+                        print("AWS was not used")
                         response = "go to sentiment analysis"
                         # sent, conf = sentiment.analyze(speech)
                         # response = f"Sentiment: {sent} \t Confidence: {conf}"
-            print('Response: ', response)
+            print("Response: ", response)
             after = time.time()
             print("Time: ", after - before)
             # send this element to AWS for response generation
@@ -199,7 +232,7 @@ def main():
             # begin the flask transfer now
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # playsound('sounds/cicoremix.mp3')
     main()
 
