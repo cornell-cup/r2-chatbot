96bc41f4f762a661de47f09ebbc2db1b6ab438d4
ss2698 <ss2698@cornell.edu>
2021-12-01 17:04:08 -0500

    experimentation with custom classes

---
+++ r2_chatterbot/util/google_speech_contexts.py
@@ -0,0 +1,17 @@
+import google.cloud.speech_v1p1beta1.types as google_speech
+# from google.cloud.speech_v1p1beta1 import enums
+from google.cloud.speech_v1p1beta1.types.resource import CustomClass
+import inspect
+
+def movement_class():
+  movement_words = ["move", "go", "turn", "rotate"]
+  name = "Movement Words"
+  custom_class_id = "movement_words"
+  class_items = list(map(lambda word: CustomClass.ClassItem(value=word), movement_words ))
+  movement_class = CustomClass(name=name, custom_class_id=custom_class_id, items=class_items)
+  return movement_class
+
+
+if __name__ == '__main__':
+  # print(list(inspect.getmembers(google_speech, inspect.isclass)))
+  movement_class()
