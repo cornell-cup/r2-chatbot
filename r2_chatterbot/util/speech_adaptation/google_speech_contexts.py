97e761783c29b90beb985bb37c5dfbac20a8c3f5
ss2698 <ss2698@cornell.edu>
2022-02-12 12:56:04 -0500

    setting up phrase sets

--- r2_chatterbot/util/speech_adaptation/google_speech_contexts.py
+++ r2_chatterbot/util/speech_adaptation/google_speech_contexts.py
@@ -1,3 +1,7 @@
+import os
+import sys
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 import google.cloud.speech_v1p1beta1.types as google_speech
 # from google.cloud.speech_v1p1beta1 import enums
 from google.cloud.speech_v1p1beta1.types.resource import CustomClass
@@ -11,6 +15,8 @@ def movement_class():
   movement_class = CustomClass(name=name, custom_class_id=custom_class_id, items=class_items)
   return movement_class
 
+
+movement_custom_class = movement_class()
 
 if __name__ == '__main__':
   # print(list(inspect.getmembers(google_speech, inspect.isclass)))
--- r2_chatterbot/util/speech_adaptation/phrase_sets.py
+++ r2_chatterbot/util/speech_adaptation/phrase_sets.py
@@ -1,11 +1,26 @@
 from google.cloud.speech_v1p1beta1.types import PhraseSet
+import google_speech_contexts
+
 
 def movement_phrases():
   """
   examples:
   
   """
-  pass
+  name = "Movement Phrases"
+  phrases = [
+    # "${movement-words} $OPERAND ${unit} ${direction-word}",
+    # "${movement-words} ${unit}  ${OPERAND} ${direction-word}",
+    "${movement-words}" #,
+    # "${direction-word}"
+  ]
+  boost_value = 10 # this may change
+  class_items = list(map(lambda word: PhraseSet.Phrase(value=word, boost=boost_value), phrases))
+  movement_phrase_set = PhraseSet(name=name, phrases=class_items, boost=boost_value)
+  return movement_phrase_set
 
 def common_phrases():
   pass
+
+
+movement_phrase_set = movement_phrases()
