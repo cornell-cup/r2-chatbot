7f878deac337debf1b5438c289f0be2fb1c2da1e
Jerry <jzg7@cornell.edu>
2022-02-19 15:49:16 -0500

    fix input_type view

--- r2_chatterbot_server/views/input_type.py
+++ r2_chatterbot_server/views/input_type.py
@@ -11,24 +11,5 @@ input_type = Blueprint('input_type', __name__, url_prefix='/input_type')
 def get_input_type():
     if request.method == 'GET':
         speech = request.args.get('speech', '')
-        print("SPEECH: ", speech)
-        data = keywords.get_topic(speech, parse_location=False)
-        print(data)
-        keywords.modify_topic_data(data, parse_location=True)
-        print(data)
-        response = "TEST"
-        if "name" in data.keys() and data["name"] == "weather":
-            keywords.modify_topic_data(data, parse_location=True)
-            api_data = weather.lookup_weather_today_city(
-                data["info"]["location"]["name"])
-            response = make_response.make_response_api(
-                data, api_data)
-        elif "name" in data.keys() and data["name"] == "restaurant":
-            keywords.modify_topic_data(data, parse_location=True)
-            api_data = restaurant.lookup_restaurant_city(
-                data["info"]["location"]["name"])
-            response = make_response.make_response_api(
-                data, api_data)
-
-
+        response = input_type(speech)
         return response
