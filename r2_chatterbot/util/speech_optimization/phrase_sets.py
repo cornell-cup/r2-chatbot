aa1a829054788ebb139def45f77af4c0353f122a
ss2698 <ss2698@cornell.edu>
2022-03-05 11:41:05 -0500

    some clean up and a temporary test file

--- r2_chatterbot/util/speech_optimization/phrase_sets.py
+++ r2_chatterbot/util/speech_optimization/phrase_sets.py
@@ -7,26 +7,17 @@ def movement_phrases():
   """
   name = "movement_phrases"
   phrases = [
-    "${movement_words} $OPERAND"
+    "${movement_words} $OPERAND",
     "${units} ${directions}",
     "${movement_words} ${units}",
     "$OPERAND ${directions}",
-    "${movement_words}", 
+    "${movement_words}",
     "${directions}"
   ]
   boost_value = 20
   class_items = list(map(lambda word: PhraseSet.Phrase(value=word), phrases))
   movement_phrase_set = PhraseSet(name=name, phrases=class_items, boost=boost_value)
   return movement_phrase_set
-
-
-def number_first_phrase():
-  name = "number_first_phrase"
-  return PhraseSet(name=name, phrases=[PhraseSet.Phrase(value="${movement_words} $OPERAND ${units} ${directions}")], boost=10)
-
-def direction_first_phrase():
-  name = "direction_first_phrase"
-  return PhraseSet(name=name, phrases=[PhraseSet.Phrase(value="${movement_words} ${units} $OPERAND ${directions}")], boost=10)
 
 def common_phrases():
   name = "common_phrases"
@@ -38,6 +29,4 @@ def common_phrases():
 
  
 movement_phrase_set = movement_phrases()
-number_first_phrase_set = number_first_phrase()
-direction_first_phrase_set = direction_first_phrase()
 common_phrase_set = common_phrases()
