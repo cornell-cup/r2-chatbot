6fd25084271d99f533c3d5bdfb7f121dc51a4fab
ss2698 <ss2698@cornell.edu>
2022-03-19 13:07:47 -0400

    cleaning up google_speech_contexts file

--- r2_chatterbot/util/speech_optimization/google_speech_contexts.py
+++ r2_chatterbot/util/speech_optimization/google_speech_contexts.py
@@ -23,15 +23,3 @@ lin_unit_class = create_custom_class(words = ["meter","meters","feet","foot"],
 
 rot_unit_class = create_custom_class(words = ["degrees", "radians"], 
   name="Rotational Units", custom_class_id="r_unit")
-
-# def create_unit_class():
-#   unit_words = ["meter","meters","feet","foot","degrees","radians"]
-#   name = "Unit Words"
-#   custom_class_id = "units"
-#   class_items = list(map(lambda word: CustomClass.ClassItem(value=word),unit_words ))
-#   unit_class = CustomClass(name=name, custom_class_id=custom_class_id,items=class_items)
-#   return unit_class
-
-# direction_custom_class = create_direction_class()
-# unit_custom_class = create_unit_class()
-
