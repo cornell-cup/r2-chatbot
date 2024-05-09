e4e315e4430f90de02f234e10944a867b69db9e5
Sayeem2004 <MohammadSSKC@gmail.com>
2024-05-09 08:36:31 -0400

    scheduler integration

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-	name="r2_chatterbot",
-	version="1.0",
-	author="Cornell CUP Robotics CS team",
-	author_email="example@example.com",
-	packages=find_packages()
-)
